e0027ba8f61fa28438f74848d0f6bc8ebe06faf5
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2022-11-15 12:00:16 +0100

    Advanced caching

--- src/main/java/com/example/ffmjava221caching/CarRestController.java
+++ src/main/java/com/example/ffmjava221caching/CarRestController.java
@@ -3,6 +3,7 @@ package com.example.ffmjava221caching;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,8 +14,13 @@ public class CarRestController {
     private final CarService service;
 
     @GetMapping
-    String makeSound() throws InterruptedException {
-        return service.makeSound();
+    String makeSound(@RequestParam String color) throws InterruptedException {
+        return service.makeSound(color) + service.makeNoise();
+    }
+
+    @GetMapping("/clearCache")
+    String clearCache() throws InterruptedException {
+        return service.clearCache();
     }
 
 }
--- src/main/java/com/example/ffmjava221caching/CarService.java
+++ src/main/java/com/example/ffmjava221caching/CarService.java
@@ -1,19 +1,35 @@
 package com.example.ffmjava221caching;
 
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
+import java.time.LocalDateTime;
 
 @Service
 public class CarService {
 
-    private static final String CACHE_KEY = "SOUND_CACHE";
+    private static final String CACHE_KEY = "CACHE_KEY";
+    private static final String CACHE_KEY_2 = "CACHE_KEY_2";
 
     @Cacheable(CACHE_KEY)
-    public String makeSound() throws InterruptedException {
+    public String makeSound(String color) throws InterruptedException {
+        System.out.println("makeSound wird aufgerufen");
         Thread.sleep(Duration.ofSeconds(7));
+        System.out.println("makeSound ist fertig");
+        return "Wrrooom of " + color + " car " + LocalDateTime.now();
+    }
 
-        return "Wrrooom";
+    @Cacheable(CACHE_KEY_2)
+    public String makeNoise() throws InterruptedException {
+        Thread.sleep(Duration.ofSeconds(5));
+
+        return "Neeeaaauuu";
+    }
+
+    @CacheEvict({CACHE_KEY, CACHE_KEY_2})
+    public String clearCache() {
+        return "Caches cleared";
     }
 }
